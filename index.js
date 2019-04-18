a12a4cab9e6cf606dd875a597480c99b2b99c7b9
Ilias Doukas <ilias@Iliass-MacBook-Pro.local>
2019-04-18 09:58:07 +0300

    uus index

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const connection = mysql.createConnection({
   database: process.env.DB_NAME
 });
 
+
+console.log('hello world');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+
 app.get('/', (req,res) => {
 
 // simple query
@@ -22,17 +30,6 @@ app.get('/', (req,res) => {
       }
   );
 });
-/*
-console.log('hello world');
-
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
 app.post('/', bodyParser.urlencoded({extended:true}), (req, res) => {
   console.log(req.body);
   res.send('POST: Hello ' + req.body.name);
@@ -44,4 +41,4 @@ app.get('/test', (req, res) => {
 });
 
 
-app.listen(3000);*/
+app.listen(3000);
