349757c0dcd92be8c914d6ff32c8903e28e5093c
Ilias Doukas <ilias@Iliass-MacBook-Pro.local>
2019-04-25 10:08:15 +0300

    login2

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ const options = {
 
 passport.use(new LocalStrategy(
     (username, password, done) => {
+      console.log('login? ${username}');
       // Normally, select * from users where username=?
       if (username !=process.env.USR && password != process.env.PWD) {
         return done(null,false);
