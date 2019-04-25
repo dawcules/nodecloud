bc146742e884232ecf3668436bc7a8a5f26d5b0c
Ilias Doukas <ilias@Iliass-MacBook-Pro.local>
2019-04-25 10:00:55 +0300

    passiparempi9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 require('dotenv').config();
+const express = require('express');
 const app = express();
 const mysql = require('mysql2');
 const passport = require('passport');
@@ -22,7 +23,6 @@ app.use(passport.session());
 
 const fs      = require('fs');
 const https   = require('https');
-const express = require('express');
 const bodyParser = require('body-parser');
 const sslkey  = fs.readFileSync('/etc/pki/tls/private/ca.key');
 const sslcert = fs.readFileSync('/etc/pki/tls/certs/ca.crt');
